1be2f48f192647d2b8814482413a39210cfcb9e4
tamakiii <tamakiii@users.noreply.github.com>
2024-05-13 22:28:40 +0900

    init HelloWorldServer

--- build.sbt
+++ build.sbt
@@ -4,5 +4,11 @@ lazy val root = project
     name := "hello-scala-recommendation",
     version := "0.1.0-SNAPSHOT",
     scalaVersion := "3.4.1",
-    libraryDependencies += "org.scalameta" %% "munit" % "0.7.29" % Test
+
+    libraryDependencies ++= Seq(
+      "com.typesafe.akka" %% "akka-http" % "10.5.3",
+      "com.typesafe.akka" %% "akka-actor-typed" % "2.8.5",
+      "com.typesafe.akka" %% "akka-stream" % "2.8.5",
+      "org.scalameta" %% "munit" % "0.7.29" % Test
+    )
   )
---
+++ src/main/scala/HelloWorldServer.scala
@@ -0,0 +1,27 @@
+import akka.actor.typed.ActorSystem
+import akka.actor.typed.scaladsl.Behaviors
+import akka.http.scaladsl.Http
+import akka.http.scaladsl.server.Directives._
+import scala.io.StdIn
+
+object HelloWorldServer {
+  def main(args: Array[String]): Unit = {
+    implicit val system = ActorSystem(Behaviors.empty, "HelloWorldSystem")
+    implicit val executionContext = system.executionContext
+
+    val route =
+      pathEndOrSingleSlash {
+        complete("Hello, World!")
+      }
+
+    val bindingFuture = Http().newServerAt("localhost", 8080).bind(route)
+
+    println(s"Server now online. Please navigate to http://localhost:8080/hello\nPress RETURN to stop...")
+    StdIn.readLine()
+    sys.addShutdownHook {
+      bindingFuture
+        .flatMap(_.unbind())
+        .onComplete(_ => system.terminate())
+    }
+  }
+}
